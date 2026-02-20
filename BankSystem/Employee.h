8d9821231b196f023aaf14c280853b65b7059d84
Nada <abdelhafezn97@gmail.com>
2026-02-20 14:18:56 +0200

    Employee 2

--- BankSystem/Employee.h
+++ BankSystem/Employee.h
@@ -1,58 +1,36 @@
 #pragma once
-#include <iostream>
-#include <string>
-using namespace std;
-class Employee
+#include "Person.h"
+class Employee: Person
 {
 protected:
-        int id;
-        string name;
-        string password;
+        
         double salary;
-    public:5
-        Employee() {
-            id = 0;
-            name = "";
-            password = "";
-            salary = 0.0;
+    public:
+        Employee():person() {
+            
+            salary = 0;
         }
-        Employee(int id, string name, string password, double salary) {
-            this->id = id;
-            this->name = name;
-            this->password = password;
-            this->salary = salary;
+        Employee(int id, string name, string password, double salary) :person(id , name , password){
+            
+            setsalary(salary);
         }
-        void setName(string name) {
-            if (Validation::isValidName(name)) {
-                this->name = name;
-            }
+       
 
-        }
-        void setPassword(string password) {
-            if (Validation::isValidPassword(password)) {
-                this->password = password;
-            }
-        }
+        
         void setSalary(double salary) {
             if (Validation::isValidSalary(salary)) {
                 this->salary = salary;
             }
         }
-        int getId() {
-            return id;
-        }
-        string getName() {
-            return name;
-        }
-        string getPassword() {
-            return password;
-        }
-        double getSalary() {
+        
+        
+        
+         double getSalary() {
             return salary;
         }
         void display() {
-            cout << "ID: " << id << endl;
-            cout << "Name: " << name << endl;
+            person::display();
+            
             cout << "Salary: " << salary << endl;
         }
 
